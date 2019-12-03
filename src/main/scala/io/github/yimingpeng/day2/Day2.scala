9fdd88ca1661d075c1c05afdae3d3abfa9e10057
Yiming <yimingpeng@kpmg.co.nz>
2019-12-03 23:31:39 +1300

    Add Day 2 Part 1, no unit test yet

--- src/main/scala/io/github/yimingpeng/day2/Day2.scala
+++ src/main/scala/io/github/yimingpeng/day2/Day2.scala
@@ -1,7 +1,7 @@
 package io.github.yimingpeng.day2
 
 import io.github.yimingpeng.Utilities.{stringToInt, txtToList}
-
+import scala.collection.mutable.ListBuffer
 import scala.math.floor
 
 object Day2 {
@@ -13,9 +13,27 @@ object Day2 {
     pos1 * pos2
   }
 
+  def runProgram(values: List[Int]): ListBuffer[Int] = {
+    var i = 0
+    var done = false
+    var valuesBuffer = values.to(collection.mutable.ListBuffer)
+    while (i < valuesBuffer.length && !done) {
+        if (valuesBuffer(i) == 1 && i+3<valuesBuffer.length) {
+          valuesBuffer.update(valuesBuffer(i+3), add(valuesBuffer(valuesBuffer(i+1)),
+            valuesBuffer(valuesBuffer(i+2))))
+          i += 4
+        }else if (valuesBuffer(i) == 2 && i+3<valuesBuffer.length) {
+          valuesBuffer.update(valuesBuffer(i+3), multiply(valuesBuffer(valuesBuffer(i+1)),
+            valuesBuffer(valuesBuffer(i+2))))
+          i += 4
+        }else if (valuesBuffer(i) == 99) {
+          done = true
+        }
+    }
+    valuesBuffer
+  }
 
   def main(args: Array[String]): Unit = {
-//      println(stringToInt(txtToList("day1.txt")).map(calculate1).sum)
-//      println(stringToInt(txtToList("day1.txt")).map(calculate2).sum)
+    println(runProgram(stringToInt(txtToList("day2.txt"))))
   }
 }
